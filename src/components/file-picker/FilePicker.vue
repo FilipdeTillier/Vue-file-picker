139a20dcd8dc3d526037617f0d23646772d42ba9
Filip de Tillier <Filip850@wp.pl>
2019-02-13 20:14:25 +0100

    project init

---
+++ src/components/file-picker/FilePicker.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        hello in file picker
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'file-picker',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
